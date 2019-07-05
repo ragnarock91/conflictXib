77f52eecd928f9f73dba193731989c413dc16236
ricardo quintero <ricardo.quintero@schibsted.com>
2019-07-05 22:05:15 +0300

    TestView implemented

---
+++ conflictXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  conflictXib
+//
+//  Created by Ricardo on 05/07/2019.
+//  Copyright © 2019 tori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
